0e56957aa7b7fba77b1d8d952f4cc7aa057f2af3
00100000 <53249541+00100000spacebar@users.noreply.github.com>
2019-11-21 20:32:20 -0500

    Update Palindrome.java

--- Projects/Project #1/Palindrome.java
+++ Projects/Project #1/Palindrome.java
@@ -2,14 +2,14 @@
 *
 * Reverses a string and returns if it's a palindrome or not
 */
-import java.util.Scanner;
+import java.util.Scanner; //imports the scanner utility
 
 public class Palindrome 
 {
-    public static void main (String[] args)
+    public static void main (String[] args) //main method
     {
         int againOrExit = 1;
-        while (againOrExit == 1) {
+        do {
             Scanner scan = new Scanner(System.in);
 
             System.out.println("Please enter any word or sentence and we'll tell you if it's a palindrome or not!");
@@ -18,19 +18,18 @@ public class Palindrome
             System.out.println("Your word/sentence backwards is: " + reverseString(userInput));
             boolean palindrome = isPalindrome(userInput);
             
-            if (palindrome = true) {
+            if (palindrome == true) {
                 System.out.println("Your word/sentence is a palindrome!");
-            } else {
+            } else if (palindrome == false) {
                 System.out.println("Your word/sentence is NOT a palindrome!");
             }
 
             System.out.println("Type 1 to re-run the program. Type 2 to exit the program.");
             againOrExit = scan.nextInt();
-
-            if (againOrExit == 2) {
-                System.out.println("Thank you for using my program <3");
-            }
-            scan.close();
+        } while (againOrExit == 1);
+        
+        if (againOrExit == 2) {
+            System.out.println("Thank you for using my program <3");
         }
     }
     public static String reverseString(String str) {
