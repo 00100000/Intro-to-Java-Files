4c82298b6a6a04acf93855acf4d2ab36e1e4f141
00100000 <53249541+00100000spacebar@users.noreply.github.com>
2019-12-02 22:35:14 -0500

    Update MailingLabel.java

--- Project List #1/MailingLabel.java
+++ Project List #1/MailingLabel.java
@@ -1,12 +1,15 @@
-import javax.swing.JOptionPane; //imports JOptionPane
+/* Author: Aidan Din    MailingLabel.java
+*
+* Takes a delimeter and a message from a user and coverts it into a mailing label
+*/
+
+import javax.swing.JOptionPane;
 
 public class MailingLabel
 {
     public static void main (String[] args)
     {
         String delimeter = JOptionPane.showInputDialog("Please enter a delimeter for your mailing address.");
-        while (delimeter.length() != 1) {
-            delimeter = JOptionPane.showInputDialog("Sorry, your delimeter must be 1 character long. Please re-enter.");
 
         String finalMessage = JOptionPane.showInputDialog("Please enter your final message with delimeters to seperate different lines.");
 
@@ -14,8 +17,13 @@ public class MailingLabel
         System.out.println(mailingLabel(finalMessage, delimeter));
         System.out.println("----------------------------");
         }
-    }
     public static String mailingLabel(String str, String del) {
-        return "WIP";
+        String finalMessage = "";
+
+        for (int index = str.length() - 1; index >= 0; index--) { //reverses a string
+            char messageChar = str.charAt(index);
+            finalMessage += messageChar;
+        }
+        return finalMessage;
     }
 }
