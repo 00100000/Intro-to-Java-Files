5384fb949ea46ceaaeaffbff58f77cf6ecac40bd
00100000 <53249541+00100000spacebar@users.noreply.github.com>
2019-12-12 08:35:40 -0500

    Update MailingLabel.java

--- Project List #1/MailingLabel.java
+++ Project List #1/MailingLabel.java
@@ -9,20 +9,26 @@ public class MailingLabel
 {
     public static void main (String[] args)
     {
+      int exitCondition = JOptionPane.YES_OPTION;
+      //user controlled exit loop
       do {
-        //gets user input
-        String delimeter = JOptionPane.showInputDialog("Please enter a delimeter for your mailing address.");
+          //gets user input
+          String delimeter = JOptionPane.showInputDialog("Please enter a delimeter for your mailing address.");
 
-        String finalMessage = JOptionPane.showInputDialog("Please enter your final message with delimeters to seperate different lines.");
+          String finalMessage = JOptionPane.showInputDialog("Please enter your final message with delimeters to seperate different lines.");
 
-        //prints final label
-        System.out.println("----------------------------");
-        System.out.println(mailingLabel(finalMessage, delimeter));
-        System.out.println("----------------------------");
+          //prints final label
+          System.out.println("----------------------------");
+          System.out.println(mailingLabel(finalMessage, delimeter));
+          System.out.println("----------------------------");
         
-        JOptionPane.showConfirmDialog(null, "Do you want to rerun the program?", JOptionPane.YES_NO_CANCEL_OPTION);
-        }
-      ) while
+          JOptionPane.showConfirmDialog(null, "Do you want to rerun the program?");
+          } while (exitCondition == JOptionPane.YES_OPTION);
+          //user controlled exit loop
+          if (exitCondition == JOptionPane.NO_OPTION) {
+            JOptionPane.showMessageDialog(null, "Goodbye!");
+          }
+    }
     public static String mailingLabel(String str, String del) { //returns a string with the delimeters representing a new line
         String replace = str.replaceAll(del, System.lineSeparator());
         return replace;
