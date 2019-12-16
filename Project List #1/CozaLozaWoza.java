06c2206add0417c53ed82add928a68b4a6a301fa
00100000 <53249541+00100000spacebar@users.noreply.github.com>
2019-12-16 08:26:36 -0500

    Update CozaLozaWoza.java

--- Project List #1/CozaLozaWoza.java
+++ Project List #1/CozaLozaWoza.java
@@ -7,50 +7,49 @@ import javax.swing.JOptionPane;
 
 public class CozaLozaWoza
 {
-    public static void main (String[] args)
-    {
-      int exitCondition = JOptionPane.NO_OPTION;
+  public static void main (String[] args)
+  {
+    int exitCondition = JOptionPane.NO_OPTION;
+    //user controlled exit loop
+    do {
+      String userInputString = JOptionPane.showInputDialog("Please input an integer."); //gets user input
+      int userInput = Integer.parseInt(userInputString);
+
+      JOptionPane.showMessageDialog(null, cozaLozaWoza(userInput)); //final output
+      JOptionPane.showConfirmDialog(null, "Do you want to rerun the program?");
+      
+    } while (exitCondition == 0);
       //user controlled exit loop
-      do {
+  }
+  public static String cozaLozaWoza(int n) { //replaces numbers divisible by 3, 5, and 7 with coza, loza, and woza respectively
+    String x = "";
         
-         String userInputString = JOptionPane.showInputDialog("Please input an integer."); //gets user input
-         int userInput = Integer.parseInt(userInputString);
+    //loops to check the divisibility of numbers
+    for (int i = 1; i <= n; i++) {
+      boolean divisible = false;
 
-         JOptionPane.showMessageDialog(null, cozaLozaWoza(userInput)); //final output
-         JOptionPane.showConfirmDialog(null, "Do you want to rerun the program?");
-         
-      } while (exitCondition == JOptionPane.YES_OPTION);
-          //user controlled exit loop
+      if (i % 3 == 0) {
+        x += "Coza";
+        divisible = true;
+      }
+      if (i % 5 == 0) {
+        x += "Loza";
+        divisible = true;
+      }
+      if (i % 7 == 0) {
+        x += "Woza";
+        divisible = true;
+      }
+      if (divisible == false) {
+        x += i;
+      }
+      if (i % 11 == 0) {
+        x += "\n";
+      } else {
+        x += " ";
+      }
     }
-    public static String cozaLozaWoza(int n) { //replaces numbers divisible by 3, 5, and 7 with coza, loza, and woza respectively
-      String x = "";
-        
-        //loops to check the divisibility of numbers
-        for (int i = 1; i <= n; i++) {
-          boolean divisible = false;
-
-            if (i % 3 == 0) {
-                x += "Coza";
-                divisible = true;
-            }
-            if (i % 5 == 0) {
-                x += "Loza";
-                divisible = true;
-            }
-            if (i % 7 == 0) {
-                x += "Woza";
-                divisible = true;
-            }
-            if (divisible == false) {
-                x += i;
-            }
-            if (i % 11 == 0) {
-                x += "\n";
-            } else {
-                x += " ";
-            }
-        }
-        //returns the final String
-        return x;
-    }
+    //returns the final String
+    return x;
+  }
 }
