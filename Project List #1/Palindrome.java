fca435e6950dd246dc0627619044421cc1733d98
00100000 <53249541+00100000spacebar@users.noreply.github.com>
2019-12-16 08:27:13 -0500

    Update Palindrome.java

--- Project List #1/Palindrome.java
+++ Project List #1/Palindrome.java
@@ -1,58 +1,54 @@
 /* Author: Aidan Din    Palindrome.java
 *
-* Reverses a string and returns if it's a palindrome or not
+*  Reverses a string and returns if it's a palindrome or not
 */
 
 import javax.swing.JOptionPane;
 
 public class Palindrome
 {
-    public static void main (String[] args)
-    {
-        int againOrExit = 1;
-        do { //user controlled exit loop
-
-            String userInput = JOptionPane.showInputDialog("Please enter any word and we'll tell you if it's a palindrome or not!");
-
-            String reversedString = "Your word backwards is: " + reverseString(userInput);
-            boolean palindrome = isPalindrome(userInput);
-            
-            if (palindrome == true) { //outputs if a word is a palindrome or not
-                JOptionPane.showMessageDialog(null, reversedString + ". Your word is a palindrome!");
-            } else {
-                JOptionPane.showMessageDialog(null, reversedString + ". Your word is NOT a palindrome!");
-            }
-
-            String againOrExitInput = JOptionPane.showInputDialog("Type 1 to re-run the program. Type 2 to exit the program.");
-            againOrExit = Integer.parseInt(againOrExitInput);
-            
-        } while (againOrExit == 1); //user controlled exit loop
-        
-        if (againOrExit == 2) {
-            JOptionPane.showMessageDialog(null, "Thank you for using my program <3");
-        }
+  public static void main (String[] args)
+  {
+    int exitCondition = JOptionPane.NO_OPTION;
+    //user controlled exit loop
+    do {
+      String userInput = JOptionPane.showInputDialog("Please enter any word and we'll tell you if it's a palindrome or not!");
+      
+      String reversedString = "Your word backwards is: " + reverseString(userInput);
+      boolean palindrome = isPalindrome(userInput);
+      
+      if (palindrome == true) { //outputs if a word is a palindrome or not
+        JOptionPane.showMessageDialog(null, reversedString + ". Your word is a palindrome!");
+      } else {
+        JOptionPane.showMessageDialog(null, reversedString + ". Your word is NOT a palindrome!");
+      }
+      
+      exitCondition = JOptionPane.showConfirmDialog(null, "Do you want to rerun the program?");
+      
+    } while (exitCondition == 0);
+    //user controlled exit loop
+  }
+  public static String reverseString(String str) { //returns a reversed string
+    String finalMessage = "";
+    
+    for (int index = str.length() - 1; index >= 0; index--) { //reverses a string
+      char messageChar = str.charAt(index);
+      finalMessage += messageChar;
     }
-    public static String reverseString(String str) { //returns a reversed string
-        String finalMessage = "";
-
-        for (int index = str.length() - 1; index >= 0; index--) { //reverses a string
-            char messageChar = str.charAt(index);
-            finalMessage += messageChar;
-        }
-        return finalMessage;
+    return finalMessage;
+  }
+  public static boolean isPalindrome(String str) { //checks if a string is equal to itself backwards
+    String finalMessage = "";
+    
+    for (int index = str.length() - 1; index >= 0; index--) { //reverses a string
+      char messageChar = str.charAt(index);
+      finalMessage += messageChar;
     }
-    public static boolean isPalindrome(String str) { //checks if a string is equal to itself backwards
-        String finalMessage = "";
-
-        for (int index = str.length() - 1; index >= 0; index--) { //reverses a string
-            char messageChar = str.charAt(index);
-            finalMessage += messageChar;
-        }
-
-        if (finalMessage.equalsIgnoreCase(str)) { //checks string against itself
-            return true;
-        } else {
-            return false;
-        }
+    
+    if (finalMessage.equalsIgnoreCase(str)) { //checks string against itself
+      return true;
+    } else {
+      return false;
     }
+  }
 }
