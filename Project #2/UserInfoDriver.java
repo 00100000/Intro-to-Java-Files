495d02da475780a87643bca5510617adf5a32ae1
00100000 <53249541+00100000spacebar@users.noreply.github.com>
2020-02-09 10:25:38 -0500

    Create UserInfoDriver.java

---
+++ Project #2/UserInfoDriver.java
@@ -0,0 +1,18 @@
+/*********************************************************
+  * 
+  * UserInfoDriver.java           Author: Aidan Din
+  * 
+  * Driver file for UserInfo.java
+  * 
+  **********************************************************/
+//@TODO:
+// - User exit loop
+// - JOptionPane
+import javax.swing.JOptionPane;
+
+public class UserInfoDriver {
+
+    public static void main (String[]args) {
+        User user = new User();
+    }
+}
