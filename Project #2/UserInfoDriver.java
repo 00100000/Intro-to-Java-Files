578bd24af2ef226b468041b975ac1e07594f9665
00100000 <53249541+00100000spacebar@users.noreply.github.com>
2020-02-25 08:03:29 -0500

    Update UserInfoDriver.java

--- Project #2/UserInfoDriver.java
+++ Project #2/UserInfoDriver.java
@@ -1,18 +1,42 @@
-/*********************************************************
-  * 
-  * UserInfoDriver.java           Author: Aidan Din
-  * 
-  * Driver file for UserInfo.java
-  * 
-  **********************************************************/
-//@TODO:
-// - User exit loop
-// - JOptionPane
+//
+// UserInfoDriver.java           Author: Aidan Din
+//
+// Driver file for UserInfo.java
+//
+
 import javax.swing.JOptionPane;
 
 public class UserInfoDriver {
 
     public static void main (String[]args) {
-        User user = new User();
+        String n, a, u, p;
+        String s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        //new user object
+        UserInfo userinfo = new UserInfo();
+        //user input
+        n = JOptionPane.showInputDialog("Please enter your name.");
+        a = JOptionPane.showInputDialog("Please enter your address.");
+        u = JOptionPane.showInputDialog("Please enter your username.");
+        p = JOptionPane.showInputDialog("Please enter your password.");
+        
+        //testing set methods
+        userinfo.setName(n);
+        userinfo.setAddress(a);
+        userinfo.setUsername(u);
+        userinfo.setPassword(p);
+        
+        //testing get methods
+        System.out.println(userinfo.getName());
+        System.out.println(userinfo.getAddress());
+        System.out.println(userinfo.getUsername());
+        System.out.println(userinfo.getPassword());
+        
+        //testing get methods
+        System.out.println(userinfo.encrypt(s));
+        System.out.println(userinfo.removeWhitespaceAndConvertToUpper(s));
+        System.out.println(userinfo.substitute(s));
+        System.out.println(userinfo.swapHalfsForEncrypt(s));
+        System.out.println(userinfo.swapFirst2WithLast2(s));
+        System.out.println(userinfo.swapMiddleChars(s));
     }
 }
