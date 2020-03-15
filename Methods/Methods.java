2e6a7e0481caed5d727cf808b4fb0c3b78ba8c7e
Aidan Din <00100000spacebar@gmail.com>
2020-03-15 12:11:53 -0400

    template

--- Methods/Methods.java
+++ Methods/Methods.java
@@ -2,3 +2,22 @@
  *
  * Does different advanced mathematical functions with different methods
  */
+
+import java.util.Scanner; //imports the scanner class
+
+public class Methods
+{
+    public static void main (String[] args)
+    {
+    }
+    public static int factorial(int n) {
+    }
+    public static int firstDigit(int n) {
+    }
+    public static boolean allDigitsOdd(int n) {
+    }
+    public static boolean allDigitsSame(int n) {
+    }
+    public static boolean hasMidpoint(int a, int b, int c) {
+   }
+}
