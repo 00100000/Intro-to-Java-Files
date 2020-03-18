2d7d603ed0d44373a0cbb250dca9a78efcbbfce1
Aidan Din <00100000spacebar@gmail.com>
2020-03-18 15:49:49 -0400

    set up

--- Project #3/DuckDuckGoose.java
+++ Project #3/DuckDuckGoose.java
@@ -4,3 +4,14 @@
 // Determines the winner of a game with a certain amount
 // of players and cycle length
 //
+
+import javax.swing.JOptionPane;
+
+public class DuckDuckGoose {
+    public static void main (String[]args) {
+
+    }
+    public static int playGame(boolean[] array, int cycle) {
+
+    }
+}
