3991507b2728352a47feb5836d827821e065fe8f
Aidan Din <00100000spacebar@gmail.com>
2020-03-28 20:35:10 -0400

    STYLED

--- Project #3/DuckDuckGoose.java
+++ Project #3/DuckDuckGoose.java
@@ -8,25 +8,27 @@
 import javax.swing.JOptionPane;
 
 public class DuckDuckGoose {
-	public static void main (String[]args) {
-    boolean[] arr = {true, true, true, true, true,}; //3
+  public static void main(String[] args) {
+    boolean[] arr = { true, true, true, true, true, }; // 3
     System.out.println(playGame(arr, 3));
   }
+
   public static int playGame(boolean[] array, int cycle) {
-  // returns winner, accepts player amount and cycle length
+    // returns winner, accepts player amount and cycle length
     int cycleAmount = 0;
     int ret = 0;
 
     for (int i = 0; cycleAmount < array.length; i += cycle) {
-      //forgive me lord for these nested loops
+      // forgive me lord for these nested loops
       while (array[i] == false) {
         i++;
+        i %= array.length;
       }
       if (array[i] == true) {
         array[i] = false;
       }
     }
-    
+
     for (int x = 0; x < array.length; x++) {
       if (array[x] == true) {
         ret = x;
